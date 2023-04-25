b348fe22565ed200b90e7adee0486351509d574c
NIRANJANA888 <NIRANJANA888>
2023-04-25 12:11:33 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,12 @@ function App() {
   }, [])
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center bg-gray-900">
+    <div className="h-fit sm:h-screen w-screen flex flex-col justify-center items-center bg-gray-900">
     <div className='h-[15%] w-screen border-b-2 border-orange-200 flex justify-between px-10 items-center'>
       <div className='text-white text-xl font-semibold'>Restaurant</div>
       <div onClick={() => navigate('/order')}><img src='/icon.svg' className='h-[55px] cursor-pointer' /></div>
     </div>
-    <div className='grid grid-cols-3 gap-5 h-[85%] w-[100%] mt-5 items-center justify-items-center'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 h-[85%] w-[100%] mt-5 items-center justify-items-center'>
       {
         foods.map((element, index) => {
           return <Card props={element} key={index} />
--- src/Order.jsx
+++ src/Order.jsx
@@ -37,12 +37,12 @@ export default function Order() {
       <div className="h-screen w-screen flex flex-col justify-center items-center bg-gray-900">
       <div className='h-[15%] w-screen border-b-2 border-orange-200 flex justify-between px-10 items-center'>
       <div className='text-white text-xl font-semibold'>Restaurant</div>
-      <div className='flex justify-between w-[15%] items-center text-lg'>
+      <div className='flex justify-between w-[50%] sm:w-[15%] items-center text-lg'>
         <div className='text-white'>Rs.{price}</div>
         <div onClick={order} className='bg-green-500 hover:bg-green-400 cursor-pointer text-white px-5 py-2 rounded-md'>Order</div>
       </div>
       </div>
-    <div className='grid grid-cols-3 gap-5 h-[85%] w-[100%] mt-5 items-center justify-items-center'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 h-[85%] w-[100%] mt-5 items-center justify-items-center'>
     {
         items.map((element, index) => {
           return <Card props={element} key={index} />
